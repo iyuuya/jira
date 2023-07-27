3b7b546c2d5fbf3fabe890b4c4be3958e2939979
iyuuya <i.yuuya@gmail.com>
2023-07-28 03:00:17 +0900

    refactor: remove unused code

--- cmd/jira/main.go
+++ cmd/jira/main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 
-	jira "github.com/andygrunwald/go-jira"
 	"github.com/iyuuya/go/exitcode"
 
 	"github.com/iyuuya/jira/internal/jira/cmd"
@@ -22,26 +20,4 @@ func realMain() exitcode.ExitCode {
 	}
 
 	return exitcode.ExitOK
-}
-
-func getProjects() {
-	tp := jira.BasicAuthTransport{
-		Username: os.Getenv("JIRA_USERNAME"),
-		Password: os.Getenv("JIRA_API_TOKEN"),
-	}
-
-	client, err := jira.NewClient(tp.Client(), os.Getenv("JIRA_HOST"))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	ps, _, err := client.Project.GetList()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, v := range *ps {
-		fmt.Printf("%s: %s\n", v.Key, v.Name)
-	}
-
 }
